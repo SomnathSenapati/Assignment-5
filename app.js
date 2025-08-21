bb098631dd60473b315910da51f1e2faebd088d6
SomnathSenapati <somnathsenapati5978@gmail.com>
2025-08-21 17:19:50 +0530

    done

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const ejs = require("ejs");
+const dbCon = require("./app/config/dbCon");
+const path = require("path");
+const fs = require("fs");
+const methodOverride = require("method-override");
+const session = require("express-session");
 
-require('dotenv').config()
-const express=require("express")
-const cors=require('cors')
-const ejs=require('ejs')
-const dbCon=require('./app/config/dbCon')
-const path=require('path')
-const fs=require('fs')
-const methodOverride = require('method-override');
-const session = require('express-session');
-
-dbCon()
-const app=express()
-app.use(cors())
+dbCon();
+const app = express();
+app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src * 'self' data: blob:;");
-    next();
+  res.setHeader("Content-Security-Policy", "default-src * 'self' data: blob:;");
+  next();
 });
 
-app.use(session({
-  secret: 'myquizsecret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } 
-}));
+app.use(
+  session({
+    secret: "sjoemrnrayth",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.set("view engine", "ejs")
-app.set("views", "views")
-
-app.use(express.static(__dirname + '/public'));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use(express.static(__dirname + "/public"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //auth route
-const userAuthRoues=require('./app/routes/userAuthRoutes')
-app.use ('/api/user',userAuthRoues)
+const userAuthRoues = require("./app/routes/userAuthRoutes");
+app.use("/api/user", userAuthRoues);
 //movie route
-const movieRoute = require('./app/routes/movieRoutes')
-app.use( '/api/movie',movieRoute)
+const movieRoute = require("./app/routes/movieRoutes");
+app.use("/api/movie", movieRoute);
 //theater route
 
-const theaterRoute=require('./app/routes/theaterRoutes')
-app.use('/api/theater',theaterRoute)
+const theaterRoute = require("./app/routes/theaterRoutes");
+app.use("/api/theater", theaterRoute);
 
 //booking route
-const bookingRoutes=require('./app/routes/bookingRoutes')
-app.use('/api/ticket',bookingRoutes)
+const bookingRoutes = require("./app/routes/bookingRoutes");
+app.use("/api/ticket", bookingRoutes);
 
-const PORT=process.env.PORT || 2809
+const PORT = process.env.PORT || 2809;
 
-app.listen(PORT,()=>{
-    console.log("sever is running at port:",PORT)
-})
+app.listen(PORT, () => {
+  console.log("sever is running at port:", PORT);
+});
